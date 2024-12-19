5536fe93731fa17e76375966ce75f8bbabfdcc51
Arman <mdarman365506@gmail.com>
2024-12-19 14:42:55 +0600

    added message text on middle

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -18,7 +18,6 @@ const Chat = () => {
             <span>Arman Mia</span>
             <p>Lorem ipsum, dolor </p>
           </div>
-
         </div>
         <div className="icons">
           <img src="./phone.png" alt="" />
@@ -26,19 +25,91 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              accusamus officiis quas, repellendus inventore nulla minus ipsum
+              vel quos porro, veritatis eligendi praesentium dicta dignissimos
+              labore velit nisi natus laudantium?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              accusamus officiis quas, repellendus inventore nulla minus ipsum
+              vel quos porro, veritatis eligendi praesentium dicta dignissimos
+              labore velit nisi natus laudantium?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              accusamus officiis quas, repellendus inventore nulla minus ipsum
+              vel quos porro, veritatis eligendi praesentium dicta dignissimos
+              labore velit nisi natus laudantium?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              accusamus officiis quas, repellendus inventore nulla minus ipsum
+              vel quos porro, veritatis eligendi praesentium dicta dignissimos
+              labore velit nisi natus laudantium?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/000/517/088/original/vector-landscape-illustration.png"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              accusamus officiis quas, repellendus inventore nulla minus ipsum
+              vel quos porro, veritatis eligendi praesentium dicta dignissimos
+              labore velit nisi natus laudantium?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <img src="./img.png" alt="" />
           <img src="./camera.png" alt="" />
           <img src="./mic.png" alt="" />
         </div>
-        <input onChange={e=>setText(e.target.value)} type="text" name="" id="" placeholder="Type a message..." value={text} />
+        <input
+          onChange={(e) => setText(e.target.value)}
+          type="text"
+          name=""
+          id=""
+          placeholder="Type a message..."
+          value={text}
+        />
         <div className="emoji">
-          <img onClick={() => setOpen((prev) => !prev)} src="./emoji.png" alt="" />
+          <img
+            onClick={() => setOpen((prev) => !prev)}
+            src="./emoji.png"
+            alt=""
+          />
           <div className="picker">
-
-          <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
           </div>
         </div>
         <button className="sendButton">Send</button>
