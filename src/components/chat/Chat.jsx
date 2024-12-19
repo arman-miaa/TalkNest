8b2c38a324c4bc893544bea80da70614c66301c4
Arman <mdarman365506@gmail.com>
2024-12-19 12:52:01 +0600

    added bottom section on center and emoji option

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,48 @@
+import EmojiPicker from "emoji-picker-react";
 import "./chat.css";
-
+import { useState } from "react";
 const Chat = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+  const handleEmoji = e => {
+    setText((prev => prev + e.emoji))
+    setOpen(false)
+  }
+ 
   return (
     <div className="chat">
-      <h1>Chat</h1>
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Arman Mia</span>
+            <p>Lorem ipsum, dolor </p>
+          </div>
+
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input onChange={e=>setText(e.target.value)} type="text" name="" id="" placeholder="Type a message..." value={text} />
+        <div className="emoji">
+          <img onClick={() => setOpen((prev) => !prev)} src="./emoji.png" alt="" />
+          <div className="picker">
+
+          <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
     </div>
   );
 };
